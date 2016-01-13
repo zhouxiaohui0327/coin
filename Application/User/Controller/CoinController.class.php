d3e95371fb08931ed63f305bd7d2a0797653d146
zhouxiaohui0327 <475503798@qq.com>
2016-01-13 17:39:32 +0800

    modify

--- Application/User/Controller/CoinController.class.php
+++ Application/User/Controller/CoinController.class.php
@@ -152,4 +152,17 @@ class CoinController extends Controller{
 
         $this->display();
     }
+
+    public function skip(){
+
+
+        $coin_id = I('get.coin_id');
+        $coin =M('coin');
+        $map['id'] = $coin_id;
+        $coinInfo = $coin -> where($map) -> find();
+
+        $this->assign('coinInfo',$coinInfo);
+
+        $this->display();
+    }
 }
