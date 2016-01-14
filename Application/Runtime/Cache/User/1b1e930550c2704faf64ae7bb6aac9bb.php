30d2e3c90a45dab116da49ce47abfdc285b62283
zhouxiaohui0327 <475503798@qq.com>
2016-01-15 03:11:48 +0800

    modify

--- Application/Runtime/Cache/User/1b1e930550c2704faf64ae7bb6aac9bb.php
+++ Application/Runtime/Cache/User/1b1e930550c2704faf64ae7bb6aac9bb.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<?php if (!defined('THINK_PATH')) exit();?><!--<!DOCTYPE html>-->
 <html>
 <head lang="en">
     <meta charset="UTF-8">
@@ -17,32 +17,62 @@
 
 <div class="container" style="margin-top: 20px">
     <div class="coin_wrap">
-        <table class="table table-bordered table-striped">
+        <table class="table table-bordered table-striped table-hover">
             <thead>
             <tr>
                 <th>id</th>
+                <th>纪念币名称</th>
                 <th>预约开始时间</th>
                 <th>预约结束时间</th>
                 <th>兑换开始时间</th>
                 <th>兑换结束时间</th>
                 <th>央行公告链接</th>
+                <th>操作</th>
             </tr>
             </thead>
             <tbody>
             <?php if(is_array($coinInfo)): foreach($coinInfo as $key=>$vo): ?><tr>
                     <td><?php echo ($vo["id"]); ?></td>
+                    <td><?php echo ($vo["name"]); ?></td>
                     <td><?php echo ($vo["apply_start"]); ?></td>
                     <td><?php echo ($vo["apply_end"]); ?></td>
                     <td><?php echo ($vo["exchange_start"]); ?></td>
                     <td><?php echo ($vo["exchange_end"]); ?></td>
                     <td><?php echo ($vo["notice_url"]); ?></td>
+                    <td>
+                        <a class="btn btn-default btn-sm" modifyId="<?php echo ($vo["id"]); ?>" id="modifyBtn" style="padding:0 10px" href="">修改</a>
+                        <a class="btn btn-danger btn-sm" style="padding:0 10px" href="">删除</a>
+                    </td>
                 </tr><?php endforeach; endif; ?>
-
             </tbody>
         </table>
     </div>
+    <script>
+        $(document).ready(function(){
+            $("#modifyBtn").click(function(e){
+                var id = $(this).attr('modifyId');
+                e.preventDefault();
+                $.get('/index.php/Index/modifyPost',{id:id},function(data){
+                    $('input[name=coin_name]').val(data['coin_name']);
+                    $('input[name=apply_start]').val(data['apply_start']);
+                    $('input[name=apply_end]').val(data['apply_end']);
+                    $('input[name=exchange_start]').val(data['exchange_start']);
+                    $('input[name=exchange_end]').val(data['exchange_end']);
+                    $('input[name=notice_url]').val(data['notice_url']);
+                    $('input[name=bank_name]').val(data['bank_name']);
+                    $('input[name=apply_url]').val(data['apply_url']);
+                    $('input[name=query_url]').val(data['query_url']);
+                    console.log(data);
+                    $('#addBtn').html("确认修改");
+                },'json');
+            })
+
+
+
+        })
+    </script>
     <div class="bank_wrap">
-        <table class="table table-bordered table-striped">
+        <table class="table table-bordered table-striped table-hover">
             <thead>
             <tr>
                 <th>id</th>
@@ -56,7 +86,7 @@
             <?php if(is_array($bankInfo)): foreach($bankInfo as $key=>$vo): ?><tr>
                     <td><?php echo ($vo["coin_id"]); ?></td>
                     <td><?php echo ($vo["id"]); ?></td>
-                    <td><?php echo ($vo["name"]); ?></td>
+                    <td><input name="xxx" bank_id="<?php echo ($vo["id"]); ?>" type="text" value="<?php echo ($vo["name"]); ?>"/></td>
                     <td><?php echo ($vo["apply_url"]); ?></td>
                     <td><?php echo ($vo["query_url"]); ?></td>
                 </tr><?php endforeach; endif; ?>
@@ -64,15 +94,36 @@
             </tbody>
         </table>
     </div>
+
+    <script>
+
+        $(document).ready(function(){
+            $("input[name=xxx]").blur(function(){
+                var bank_name = $(this).val();
+                var bank_id =$(this).attr("bank_id");
+                $.get('/index.php/Index/modify',{bank_name:bank_name,bank_id:bank_id},function(data){
+                    if(data.status){
+                        alert(data.info);
+                    }else{
+                        alert(data.info);
+                    }
+                }
+              ,'json')
+            })
+        })
+
+    </script>
+
+
     <div class="area_wrap">
 
-        <table class="table table-bordered table-striped">
+        <table class="table table-bordered table-striped table-hover">
             <tr>
                 <th>银行编号</th>
                 <th colspan="8">地区</th>
             </tr>
 
-            <?php $__FOR_START_29978__=1;$__FOR_END_29978__=5;for($i=$__FOR_START_29978__;$i < $__FOR_END_29978__;$i+=1){ ?><tr>
+            <?php $__FOR_START_29188__=1;$__FOR_END_29188__=$count+1;for($i=$__FOR_START_29188__;$i < $__FOR_END_29188__;$i+=1){ ?><tr>
                     <td><?php echo ($i); ?></td>
                     <?php if(is_array($areaInfo[$i])): foreach($areaInfo[$i] as $key=>$vo): ?><td><?php echo ($vo["area"]); ?></td><?php endforeach; endif; ?>
                 </tr><?php } ?>
@@ -80,6 +131,79 @@
         </table>
     </div>
 </div>
+
+
+<div>
+    <form class="form-horizontal" action="/index.php/Index/addPost">
+        <div class="form-group" style="margin-right: 0">
+            <label class="col-sm-2 control-label">纪念币信息</label>
+            <div class="col-sm-2">
+                <input type="text" class="form-control" name="coin_name" placeholder="纪念币信息">
+            </div>
+        </div>
+        <div class="form-group" style="margin-right: 0">
+            <label class="col-sm-2 control-label">预约开始时间</label>
+            <div class="col-sm-2">
+                <input type="date" class="form-control" name="apply_start" placeholder="预约开始时间">
+            </div>
+        </div>
+        <div class="form-group" style="margin-right: 0">
+            <label class="col-sm-2 control-label">预约结束时间</label>
+            <div class="col-sm-2">
+                <input type="date" class="form-control" name="apply_end" placeholder="预约结束时间">
+            </div>
+        </div>
+        <div class="form-group" style="margin-right: 0">
+            <label class="col-sm-2 control-label">兑换开始时间</label>
+            <div class="col-sm-2">
+                <input type="date" class="form-control" name="exchange_start" placeholder="兑换开始时间">
+            </div>
+        </div>
+        <div class="form-group" style="margin-right: 0">
+            <label class="col-sm-2 control-label">兑换结束时间</label>
+            <div class="col-sm-2">
+                <input type="date" class="form-control" name="exchange_end" placeholder="兑换结束时间">
+            </div>
+        </div>
+        <div class="form-group" style="margin-right: 0">
+            <label class="col-sm-2 control-label">央行公告链接</label>
+            <div class="col-sm-5">
+                <input type="text" class="form-control" name="notice_url" placeholder="央行公告链接">
+            </div>
+        </div>
+        <div class="form-group" style="margin-right: 0">
+            <label class="col-sm-2 control-label">兑换银行</label>
+            <div class="col-sm-5">
+                <input type="text" class="form-control" name="bank_name" placeholder="多个银行用“，”隔开">
+            </div>
+        </div>
+        <div class="form-group" style="margin-right: 0">
+            <label class="col-sm-2 control-label">预约链接</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" name="apply_url" placeholder="多个预约链接用“，”隔开">
+            </div>
+        </div>
+        <div class="form-group" style="margin-right: 0">
+            <label class="col-sm-2 control-label">查询链接</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" name="query_url" placeholder="多个查询链接用“，”隔开">
+            </div>
+        </div>
+        <div class="form-group" style="margin-right: 0">
+            <label class="col-sm-2 control-label">地区</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" name="area_names" placeholder="多个地区用“，”隔开 ， 多个银行用“；”隔开">
+            </div>
+        </div>
+        <div class="form-group" style="margin-right: 0">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" id="addBtn" class="btn btn-success">确认添加</button>
+            </div>
+        </div>
+    </form>
+</div>
+
+
 
 
 </body>
--- Application/User/Controller/IndexController.class.php
+++ Application/User/Controller/IndexController.class.php
@@ -42,6 +42,9 @@ class IndexController extends Controller {
          * 循环出area表数据
          */
 
+        $count = count($bankInfo);
+
+
         $areaModel =M('area');
 
         for($i=1;$i<count($bankInfo)+1;$i++){
@@ -49,6 +52,7 @@ class IndexController extends Controller {
         }
 
         $this -> assign('areaInfo',$areaInfo);
+        $this -> assign('count',$count);
 
         $this->display();
 
@@ -77,22 +81,7 @@ class IndexController extends Controller {
         $user = M('User');
         $map['account'] = $account;
         $userModel =$user->where($map)->find();
-//        if(!empty($userModel))
-//        {
-//            if($userModel['password'] == md5($password))
-//            {
-//                $this->ajaxReturn('','登录成功',0);
-//                setcookie("user", "$account", time()+3600);
-//            }
-//            else
-//            {
-//                $this->ajaxReturn('','密码不正确',4);
-//            }
-//        }
-//        else
-//        {
-//            $this->ajaxReturn('','用户名不存在',3);
-//        }
+
 
         if(!empty($userModel))
         {
@@ -109,10 +98,158 @@ class IndexController extends Controller {
         {
             die("<script>alert('用户名不存在');history.back();</script>");
         }
+    }
 
 
 
+    public function addPost(){
+
+        header("Content-type:text/html;charset=utf8");
+        $coin_name = I('get.coin_name');
+
+        $apply_start = I('get.apply_start');
+        $apply_end = I('get.apply_end');
+        $exchange_start = I('get.exchange_start');
+        $exchange_end = I('get.apply_end');
+        $notice_url= I('get.notice_url');
+        $bank_name= I('get.bank_name');
+        $apply_url= I('get.apply_url');
+        $query_url= I('get.query_url');
+        $area_names= I('get.area_names');
+
+
+        $Coin = M('coin');
+        $data1['name']=$coin_name;
+        $data1['apply_start']=$apply_start;
+        $data1['apply_end']=$apply_end;
+        $data1['exchange_start']=$exchange_start;
+        $data1['exchange_end']=$exchange_end;
+        $data1['notice_url']=$notice_url;
+
+        $CoinId = $Coin->add($data1);
+
+
+        $Bank = M('bank');
+
+        $bank_name_num =explode("，",$bank_name);
+        $apply_url_num =explode("，",$apply_url);
+        $query_url_num =explode("，",$query_url);
+
+        $count1 = count($bank_name_num);
+
+        for($i=0;$i<$count1;$i++){
+            $data2['coin_id']=$CoinId;
+            $data2['name']=$bank_name_num[$i];
+            $data2['apply_url']=$apply_url_num[$i];
+            $data2['query_url']=$query_url_num[$i];
+            $BankId[$i] = $Bank->add($data2);
+        }
+
+        $Area = M('area');
+
+        $area_names_num =explode("；",$area_names);
+        $count =count($area_names_num);
+
+        for($x=0;$x<$count1;$x++){
+            $one = explode("，",$area_names_num[$x]);
+            for($i=0;$i<$count;$i++){
+                $data3['bank_id'] =$BankId[$x];
+                $data3['area']=$one[$i];
+                $AreaResult=$Area->add($data3);
+            }
+        }
+
+        if($CoinId&&$BankId&&$AreaResult){
+            header("location:/index.php/Index/admin");
+        }else{
+            die("<script>alert('添加失败');history.back();</script>");
+        }
+    }
+
+
+    public function modifyPost(){
+        header("Content-type:text/html;charset=utf-8");
+        $id = I('get.id');
+        $map['id'] = $id;
+        $result = M('coin')->where($map)->find();
+
+        $data['coin_name'] = $result['name'];
+        $data['apply_start'] = $result['apply_start'];
+        $data['apply_end'] = $result['apply_end'];
+        $data['exchange_start'] = $result['exchange_start'];
+        $data['exchange_end'] = $result['exchange_end'];
+        $data['notice_url'] = $result['notice_url'];
+
+        $map1['coin_id']=$id;
+        $result1 =M('bank')->where($map1)->select();
+        $count = count($result1);
+
+
+        $data['bank_name']="";
+        for($i=0;$i<$count;$i++){
+//            $data['bank_name'][$i] = $result1[$i]['name'];
+//            $data['apply_url'][$i] = $result1[$i]['apply_url'];
+//            $data['query_url'][$i] = $result1[$i]['query_url'];
+
+            $data['bank_name'].=$result1[$i]['name'].",";
+            $data['apply_url'].=$result1[$i]['apply_url'].",";
+            $data['query_url'].=$result1[$i]['query_url'].",";
+        }
+
+        $data['bank_name'] = substr($data['bank_name'],0,strlen($data['bank_name'])-1);
+        $data['apply_url'] = substr($data['apply_url'],0,strlen($data['apply_url'])-1);
+        $data['query_url'] = substr($data['query_url'],0,strlen($data['query_url'])-1);
+
+
+
+        $data['area']="";
+        for($i=0;$i<$count;$i++){
+            $map2['bank_id'] =$result1[$i]['id'];
+            $result2[$i] = M('area')->where($map2)->select();
+            $count2 = count($result2[$i]);
+            for($x=0;$x<$count2;$x++){
+                $data['area'][$i][$x] = $result2[$i][$x]['area'];
+           }
+        }
+
+//        $count111 =count($data['area']);
+//
+//        for($i=0;$i<$count111;$i++){
+//            $count222 =count($data['area'][$i]);
+//
+//            for($x=0;$x<$count222;$x++){
+//               $data['area'][]= $data['area'][$i][$x].",";
+//
+//            }
+//        }
+//        print_r($data['area']);
+//
+//die();
+        $this->ajaxReturn($data);
+    }
+
+    public function modify(){
+        header("Content-type:text/html;charset=utf8");
+
+        $bank_name=I('get.bank_name');
+        $bank_id=I('get.bank_id');
+
+
+        $Bank = M('bank');
+        $data['name'] = $bank_name;
+        $map['id'] = $bank_id;
+        $result =$Bank->where($map)->save($data);
+
+
+        if(is_int($result)){
+            $this->ajaxReturn('','修改成功',1);
+        }else{
+            $this->ajaxReturn('','修改失败',0);
+        }
 
 
     }
+
+
+
 }
