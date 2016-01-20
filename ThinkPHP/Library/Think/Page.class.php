83e478b65fc74a6d2311cd655681ba632f62431a
zhouxiaohui0327 <475503798@qq.com>
2016-01-20 22:12:58 +0800

    modify

--- ThinkPHP/Library/Think/Page.class.php
+++ ThinkPHP/Library/Think/Page.class.php
@@ -85,7 +85,7 @@ class Page{
 
         /* 生成URL */
         $this->parameter[$this->p] = '[PAGE]';
-        $this->url = explode('.',U(ACTION_NAME, $this->parameter))[0];
+        $this->url= $xxx[0]= explode('.',U(ACTION_NAME, $this->parameter));
         /* 计算分页信息 */
         $this->totalPages = ceil($this->totalRows / $this->listRows); //总页数
         if(!empty($this->totalPages) && $this->nowPage > $this->totalPages) {
